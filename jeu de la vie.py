72461691679397690bef938a76ff467cde2f8793
BenjaminArhancet <barhancet@gaming.tech>
2022-11-14 14:57:07 +0100

    new

--- jeu de la vie.py
+++ jeu de la vie.py
@@ -11,8 +11,8 @@ def plateau (x):
     tab2.append(tab)
   return tab2
  
-tab = plateau(4)
- 
+tab = plateau(5)
+
 def displayTable(tab):
     for i in tab:
         print(i)
@@ -22,56 +22,56 @@ displayTable(tab)
  
 #Fontion qui vérifie si il y a un X au dessus de la case selectionée en paramètre
 def checkUp (tab,x,y):
-  if y == 0 :
+  if x == 0 :
     return None
   else :
     return tab[x-1][y]
  
 #Fontion qui vérifie si il y a un X a gauche de la case selectionée en paramètre
 def checkLeft (tab,x,y):
-  if x == 0 :
+  if y == 0 :
     return None
   else :
     return tab[x][y-1]
  
 #Fontion qui vérifie si il y a un X a droite de la case selectionée en paramètre
 def checkRight (tab,x,y):
-  if x == len(tab) :
+  if y == (len(tab)-1) :
     return None
   else :
     return tab[x][y+1]
  
 #Fontion qui vérifie si il y a un X en dessous de la case selectionée en paramètre
 def checkDown (tab,x,y):
-  if y == len(tab):
+  if x == (len(tab)-1):
     return None
   else :
     return tab[x+1][y]
 
 #Fontion qui vérifie ce qu'il y a au dessus à gauche de la case selectionée en paramètre
 def checkUpLeft (tab,x,y):
-  if y == 0 and x == 0:
+  if y == 0 or x == 0:
     return None
   else :
     return tab[x-1][y-1]
 
 #Fontion qui vérifie ce qu'il y a au dessus à droite de la case selectionée en paramètre
 def checkUpRight (tab,x,y):
-  if y == 0 and x == len(tab):
+  if y == (len(tab)-1) or x == 0:
     return None
   else :
     return tab[x-1][y+1]
 
 #Fontion qui vérifie ce qu'il y a au dessous à gauche de la case selectionée en paramètre
 def checkDownLeft (tab,x,y):
-  if y == len(tab) and x == 0:
+  if x == (len(tab)-1) or y == 0:
     return None
   else :
     return tab[x+1][y-1]
 
 #Fontion qui vérifie ce qu'il y a au dessous à droite de la case selectionée en paramètre
 def checkDownRight (tab,x,y):
-  if y == len(tab) and x == len(tab):
+  if y == (len(tab)-1) or x == (len(tab)-1):
     return None
   else :
     return tab[x+1][y+1]
@@ -108,11 +108,18 @@ def checkLife (tab,x,y) :
 def fullCheck (tab) :
   for i in range (len(tab)) :
     for j in range (len(tab)) :
-      checkLife (tab,i,j) 
+      checkLife (tab,i,j)
 
 fullCheck(tab)
 
+print("\n")
+displayTable(tab)
+
+fullCheck(tab)
+
+print("\n")
 displayTable(tab)
 
 print(checkUp(tab,1,1))
 print(checkDown(tab,1,1))
+print(checkRight(tab,0,4))
